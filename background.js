8ba8ab3d56d64934bbe260d62db7d6ac3e3bd5a3
smolyoshino <coolguygameplayer52@gmail.com>
2022-04-19 11:48:12 -0400

    initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+// open page upon install instructing users how to use this piece of shit
+// https://stackoverflow.com/questions/5745822/open-a-help-page-after-chrome-extension-is-installed-first-time/24429523#24429523
+// console.warn("nuts nuts nuts");
+// console.error("nuts nuts nuts");
+chrome.runtime.onInstalled.addListener(function (object) {
+    let internalUrl = chrome.runtime.getURL("instructions.html");
+
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({ url: internalUrl }, function (tab) {
+            console.log("New tab launched with instructions.");
+        });
+    }
+});
